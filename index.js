49b45f339ee89e9d46641c77d6c5e2a3238aa5fd
Joel <ndohjoelmbj16@gmail.com>
2023-08-23 22:31:01 +0100

    feat: socket-connection is complete

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { app } = require('./src/app');
 const { connectToDatabase } = require('./src/database/connection');
+const { socketAuth } = require('./src/middlewares/protect');
 require('dotenv').config();
 
 // eslint-disable-next-line no-undef
@@ -8,7 +9,26 @@ const PORT = process.env.PORT || 7001;
 // Connect to database
 connectToDatabase();
 
-
-app.listen(PORT, async() => {
+const server = app.listen(PORT, async () => {
   console.log('Server is running on port ' + PORT);
+});
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PURGE'],
+  },
+});
+
+io.use(socketAuth);
+
+io.on('connection', async (socket) => {
+  console.log('🔥 User connected:', socket.user.uid);
+  socket.io = io;
+
+  app.set('socket', socket);
+
+  socket.on('disconnect', async () => {
+    console.log('🧯 User disconnected: ', socket.user.uid);
+  });
 });
